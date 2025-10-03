14ec1d1021ef7b16ef6425a7f5500a4d8a5855e9
unknown <mariuta.ciprian@iisghisleri.local>
2025-10-03 13:43:47 +0200

    yt

--- js/code.js
+++ js/code.js
@@ -1,5 +1,8 @@
 function login_funct(){
-    var username= document.getElementById('utente');
-    var password= document.getElementById('pass');
-    
+    //values
+    var username= document.getElementById('utente').value;
+    var password= document.getElementById('pass').value;
+    //classi
+    var classe_username=document.getElementById('utente');
+    var classe_password=document.getElementById('pass');
 }
